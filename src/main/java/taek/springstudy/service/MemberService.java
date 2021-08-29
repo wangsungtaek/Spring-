dd8b513c4925f50f2a6dd42aa15bf82d6777d16f
wangsungtaek <tjdxorv@naver.com>
2021-08-29 23:02:19 +0900

    Spring 입문 1~13강

---
+++ src/main/java/taek/springstudy/service/MemberService.java
@@ -0,0 +1,42 @@
+package taek.springstudy.service;
+
+import taek.springstudy.domain.Member;
+import taek.springstudy.repository.MemberRepository;
+import taek.springstudy.repository.MemoryMemberRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public class MemberService {
+
+    private final MemberRepository memberRepository = new MemoryMemberRepository();
+
+    /**
+     * 회원가입
+     */
+    public Long join(Member member) {
+
+        validateDuplicateMember(member);
+        memberRepository.save(member);
+        return member.getId();
+    }
+
+    private void validateDuplicateMember(Member member) {
+        memberRepository.findByName(member.getName())
+            .ifPresent(m -> {
+                throw new IllegalStateException("이미 존재하는 회원입니다.");
+            });
+    }
+
+    /**
+     * 전체 회원 조회
+     */
+    public List<Member> findMembers() {
+        return memberRepository.findAll();
+    }
+
+    public Optional<Member> findOne(Long memberId) {
+        return memberRepository.findById(memberId);
+    }
+
+}
---
+++ src/test/java/taek/springstudy/service/MemberServiceTest.java
@@ -0,0 +1,22 @@
+package taek.springstudy.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MemberServiceTest {
+
+
+
+    @Test
+    void 회원가입() {
+    }
+
+    @Test
+    void findMembers() {
+    }
+
+    @Test
+    void findOne() {
+    }
+}
