50943e19e7f8eaf901dcea2295bc23ad184a3ff3
wangsungtaek <tjdxorv@naver.com>
2021-09-01 23:11:48 +0900

    Spring 입문 21강

--- src/main/java/taek/springstudy/SpringConfig.java
+++ src/main/java/taek/springstudy/SpringConfig.java
@@ -1,22 +1,31 @@
 package taek.springstudy;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import taek.springstudy.repository.JdbcMemberRepository;
 import taek.springstudy.repository.MemberRepository;
-import taek.springstudy.repository.MemoryMemberRepository;
 import taek.springstudy.service.MemberService;
+
+import javax.sql.DataSource;
 
 @Configuration
 public class SpringConfig {
 
+    private DataSource dataSource;
+
+    @Autowired
+    public SpringConfig(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
     @Bean
     public MemberService memberService() {
         return new MemberService(memberRepository());
-
     }
 
     @Bean
     public MemberRepository memberRepository() {
-        return new MemoryMemberRepository();
+        return new JdbcMemberRepository(dataSource);
     }
 }
